ed091237a7ad1fe641129edd9bfc819e6d85ab05
Rishav Maskey <rishav95maskey@gmail.com>
2022-02-09 20:55:15 +0545

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <h1>Col1</h1>
+          </div>
+
+          <div className="col">
+            <h1>Col2</h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-4">
+            <h2>Col 4</h2>
+          </div>
+
+          <div className="col-8">
+            <h2>Col 8</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
