c2c6e5b3e2a34d30e84ac730f3e6a195c27b0e83
Abhijeet Pradhanang <abhipradhanang@gmail.com>
2022-02-09 23:11:00 +0545

    style: add home component

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,12 @@ import "../assets/scss/pages/_home.scss";
 function Home() {
   return (
     <div className="p-home">
+      <div className="p-home__toolbar">
+        <h1>Homepage</h1>
+
+        <button class="btn btn-success">Button</button>
+      </div>
+
       <Contact />
 
       <Grid />
